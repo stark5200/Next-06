83d5287498b4f682d43ab7682269def7f8d76a4b
stark5200 <a.taha5200@gmail.com>
2023-06-02 01:32:48 +0200

    Setup complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          I&apos;m <span className="font-bold">Ammar</span>.
+        </span>
+      </p>
       
     </main>
   )
